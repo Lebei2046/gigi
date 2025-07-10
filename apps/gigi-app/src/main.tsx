71f83e8b8b5ac5f23322ab6d4532e49cd22c19c3
Lebei <lebei163@163.com>
2025-07-10 15:11:38 +0800

    adopt RTK as state management

--- apps/gigi-app/src/main.tsx
+++ apps/gigi-app/src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './store'
 import App from './App'
 import './index.css'
 import 'virtual:uno.css'
@@ -8,6 +10,8 @@ const container = document.getElementById('root')
 const root = createRoot(container!)
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
