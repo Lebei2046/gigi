15e7f4653811abc4729af14a054ec7447ae961f1
Lebei <lebei163@163.com>
2025-07-14 19:53:56 +0800

    refactor home page

--- apps/gigi-app/src/features/home/Home.tsx
+++ apps/gigi-app/src/features/home/Home.tsx
@@ -1,11 +1,16 @@
 import { useState } from 'react';
+import {
+  FaComment as Chat,
+  FaAddressBook as Contact,
+  FaCompass as Discover,
+  FaUser as Me
+} from 'react-icons/fa';
+
 import Dock from './components/Dock';
 import ChatList from "./views/ChatList";
 import ContactList from "./views/ContactList";
 import MePage from "./views/MePage";
 import DiscoverPage from "./views/DiscoverPage";
-
-import { FaHome, FaSearch, FaMusic, FaUser } from 'react-icons/fa';
 
 export default function Home() {
   const [activeTab, setActiveTab] = useState('chat');
@@ -38,29 +43,35 @@ export default function Home() {
         <Dock.List className="dock-bottom p-2 bg-base-100 bg-opacity-90 backdrop-blur-sm shadow-lg z-50">
           <Dock.Trigger value="chat" className="dock-item">
             <div className="dock-label">
-              <FaHome className="dock-icon" />
-              <span className="dock-text">聊天</span>
+              <Chat className="dock-icon text-xl" />
+              <span className="dock-text text-lg">聊天</span>
             </div>
           </Dock.Trigger>
 
           <Dock.Trigger value="contact" className="dock-item">
             <div className="dock-label">
-              <FaSearch className="dock-icon" />
-              <span className="dock-text">通讯录</span>
+              <div className="flex flex-col items-center">
+                <Contact className="dock-icon text-xl" />
+                <span className="dock-text text-lg">通讯录</span>
+              </div>
             </div>
           </Dock.Trigger>
 
           <Dock.Trigger value="discover" className="dock-item">
             <div className="dock-label">
-              <FaMusic className="dock-icon" />
-              <span className="dock-text">发现</span>
+              <div className="flex flex-col items-center">
+                <Discover className="dock-icon text-xl" />
+                <span className="dock-text text-lg">发现</span>
+              </div>
             </div>
           </Dock.Trigger>
 
           <Dock.Trigger value="me" className="dock-item">
             <div className="dock-label">
-              <FaUser className="dock-icon" />
-              <span className="dock-text">我</span>
+              <div className="flex flex-col items-center">
+                <Me className="dock-icon text-xl" />
+                <span className="dock-text text-lg">我</span>
+              </div>
             </div>
           </Dock.Trigger>
         </Dock.List>
