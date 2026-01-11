ce63e5f94b77827c54d67748b83ae49a3919fb42
Lebei <lebei2046@gmail.com>
2026-01-11 08:57:35 +0800

    gigi-dns: log level from cmd

--- pkgs/gigi-dns/examples/simple.rs
+++ pkgs/gigi-dns/examples/simple.rs
@@ -40,9 +40,11 @@ struct Args {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::DEBUG)
-        .init();
+    // Respect RUST_LOG environment variable, default to INFO level
+    let env_filter = tracing_subscriber::EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info"));
+
+    tracing_subscriber::fmt().with_env_filter(env_filter).init();
 
     let args = Args::parse();
 
